7ace877245911a50900cf18ebf6c46c9f1300077
starlitgateway-lang <starlitgateway@gmail.com>
2026-04-23 21:17:31 -0700

    Fix authorization header format in poll.js

--- api/poll.js
+++ api/poll.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   
   const response = await fetch(`https://api.replicate.com/v1/predictions/${id}`, {
     headers: {
-      'Authorization': `r8_9XdMegrSNNwa9noZrJ3cYOKn6Bm0Vb71fV8iQ`,
+      'Authorization': `Token r8_9XdMegrSNNwa9noZrJ3cYOKn6Bm0Vb71fV8iQ`,
     }
   });
   
