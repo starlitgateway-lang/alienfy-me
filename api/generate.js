6edd2905dd7fe3911f55bdd7ff639f922a37bdb2
starlitgateway-lang <starlitgateway@gmail.com>
2026-04-23 20:43:35 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const response = await fetch('https://api.replicate.com/v1/predictions', {
       method: 'POST',
       headers: {
-        'Authorization': `Token ${process.env.REPLICATE_API_TOKEN}`,
+        'Authorization': `r8_8NqXJLMVio4698ZaRpwP30fD12vYWqJ33DKtV`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
