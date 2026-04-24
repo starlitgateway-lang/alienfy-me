c44d576bdc7ad5720d674a8a9262e00551dc125a
starlitgateway-lang <starlitgateway@gmail.com>
2026-04-23 20:52:58 -0700

    Add API handler for fetching predictions

---
+++ api/poll.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  const { id } = req.query;
+  
+  const response = await fetch(`https://api.replicate.com/v1/predictions/${id}`, {
+    headers: {
+      'Authorization': `Token r8_YOUR_ACTUAL_TOKEN_HERE`,
+    }
+  });
+  
+  const data = await response.json();
+  res.status(200).json(data);
+}
