240193bec7e35e019908d0a5da1080ea3d640b78
starlitgateway-lang <starlitgateway@gmail.com>
2026-04-24 20:57:54 -0700

    Update API token in poll.js

--- api/poll.js
+++ api/poll.js
@@ -2,13 +2,14 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
   const { id } = req.query;
-  
+
   const response = await fetch(`https://api.replicate.com/v1/predictions/${id}`, {
     headers: {
-      'Authorization': `Token r8_2ftsKYcRvxAzPHyMMHl5ercP5Gxrksf1Bhmur`,
+      'Authorization': `Token r8_PUTYOURTOKENHERE`,
+      'Content-Type': 'application/json',
     }
   });
-  
+
   const data = await response.json();
   res.status(200).json(data);
 }
